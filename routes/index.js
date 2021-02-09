40b4a08c227fb129be2929e34864df3d4c61e9f8
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-02-09 15:08:13 +1100

    Add kubevela to index route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express with Docker' });
+  res.render('index', { title: 'Express with Docker and Kubevela' });
 });
 
 module.exports = router;
